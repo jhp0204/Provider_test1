404680b5403d1e93eddd28dd0b67c6818faebc7d
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-12-16 12:27:52 +0900

    Create config.go

---
+++ Ongoing/scp/config.go
@@ -0,0 +1,47 @@
+package scp
+
+// 각 상품별 sdk 정보를 가져와서 진행, 이후 scp용 sdk 작성 후, import package 목록으로 수정, 반영 가능 
+import (
+	"time"
+
+	"github.com/NaverCloudPlatform/ncloud-sdk-go-v2/ncloud"
+	"github.com/NaverCloudPlatform/ncloud-sdk-go-v2/services/vpc"
+)
+
+// DefaultWaitForInterval is Interval for checking status in WaitForXXX method < 큰 수정 불필요 판단 
+const DefaultWaitForInterval = 10
+
+// Default timeout
+const DefaultTimeout = 5 * time.Minute
+const DefaultCreateTimeout = 1 * time.Hour
+const DefaultUpdateTimeout = 10 * time.Minute
+const DefaultStopTimeout = 5 * time.Minute
+
+type Config struct {
+	AccessKey string
+	SecretKey string
+}
+
+// 앞선 상품별 sdk를 연결, 역시 변경해야하는 부분, vpc 제외하고 전체 삭제 
+type ScpAPIClient struct {
+	vpc           *vpc.APIClient
+}
+
+func (c *Config) Client() (*NcloudAPIClient, error) {
+	apiKey := &ncloud.APIKey{
+		AccessKey: c.AccessKey,
+		SecretKey: c.SecretKey,
+	}
+// 앞선 상품별 sdk를 연결, 역시 변경해야하는 부분, vpc 제외하고 전체 삭제 	
+	return &NcloudAPIClient{
+		vpc:           vpc.NewAPIClient(vpc.NewConfiguration(apiKey)),
+	}, nil
+}
+
+type ProviderConfig struct {
+	Site       string
+	SupportVPC bool
+	RegionCode string
+	RegionNo   string
+	Client     *NcloudAPIClient
+}
