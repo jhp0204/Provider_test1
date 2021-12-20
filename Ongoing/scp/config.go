420157cc53e51b940d5f94099ff670682ab6d16a
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-12-20 12:11:23 +0900

    Update config.go

--- Ongoing/scp/config.go
+++ Ongoing/scp/config.go
@@ -17,27 +17,32 @@ const DefaultCreateTimeout = 1 * time.Hour
 const DefaultUpdateTimeout = 10 * time.Minute
 const DefaultStopTimeout = 5 * time.Minute
 
+// SCP도 AccessKey, SecretKey는 동일 
 type Config struct {
 	AccessKey string
 	SecretKey string
 }
 
 // 앞선 상품별 sdk를 연결, 역시 변경해야하는 부분, vpc 제외하고 전체 삭제 
+// ncloud vpc sdk 부분 변경 필요
 type ScpAPIClient struct {
 	vpc           *vpc.APIClient
 }
 
+// ncloud sdk 부분 변경 필요
 func (c *Config) Client() (*NcloudAPIClient, error) {
 	apiKey := &ncloud.APIKey{
 		AccessKey: c.AccessKey,
 		SecretKey: c.SecretKey,
 	}
-// 앞선 상품별 sdk를 연결, 역시 변경해야하는 부분, vpc 제외하고 전체 삭제 	
+// 앞선 상품별 sdk를 연결, 역시 변경해야하는 부분, vpc 제외하고 전체 삭제 
+// ncloud vpc sdk 부분 변경 필요
 	return &NcloudAPIClient{
 		vpc:           vpc.NewAPIClient(vpc.NewConfiguration(apiKey)),
 	}, nil
 }
 
+// ncloud vpc sdk 부분 변경 필요
 type ProviderConfig struct {
 	Site       string
 	SupportVPC bool
