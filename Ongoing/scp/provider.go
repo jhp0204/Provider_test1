a9e9291226022a33d951ba639721807eb4e75097
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-12-21 16:33:59 +0900

    Update provider.go

--- Ongoing/scp/provider.go
+++ Ongoing/scp/provider.go
@@ -33,79 +33,15 @@ func schemaMap() map[string]*schema.Schema {
 			DefaultFunc: schema.EnvDefaultFunc("SCP_SECRET_KEY", nil),
 			Description: descriptions["secret_key"],
 		},
-		"region": {
-			Type:        schema.TypeString,
-			Required:    true,
-			DefaultFunc: schema.EnvDefaultFunc("SCP_REGION", nil),
-			Description: descriptions["region"],
-		},
-		"site": {
-			Type:        schema.TypeString,
-			Optional:    true,
-			DefaultFunc: schema.EnvDefaultFunc("SCP_SITE", nil),
-			Description: descriptions["site"],
-		},
-// support_vpc의 역할 및 SCP 상품 생성 간 해당 argument가 필요한지 확인 필요
-    "support_vpc": {
-			Type:        schema.TypeBool,
-			Optional:    true,
-			DefaultFunc: schema.EnvDefaultFunc("SCP_SUPPORT_VPC", nil),
-			Description: descriptions["support_vpc"],
-		},
 	}
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	providerConfig := ProviderConfig{
-		SupportVPC: d.Get("support_vpc").(bool),
-	}
-
-	// Set site, 각 환경 별 API GW 확인 및 반영하기 (API가 GW를 통해서 들어가는 경우 한정) 
-	if site, ok := d.GetOk("site"); ok {
-		providerConfig.Site = site.(string)
-
-		switch site {
-		case "dev":
-			os.Setenv("SCP_API_GW", "개발환경 SCP API GW 입력")
-		case "stage":
-			os.Setenv("SCP_API_GW", "검증환경 SCP API GW 입력")
-		}
-	}
-
-	// Fin only supports VPC, SCP에서는 불필요 예상, 확인필요 
-	if providerConfig.Site == "fin" {
-		providerConfig.SupportVPC = true
-	}
-
-
 	// Set client
 	config := Config{
 		AccessKey: d.Get("access_key").(string),
 		SecretKey: d.Get("secret_key").(string),
 	}
-
-	if client, err := config.Client(); err != nil {
-		return nil, err
-	} else {
-		providerConfig.Client = client
-	}
-
-	// Set region
-	if err := setRegionCache(providerConfig.Client, providerConfig.SupportVPC); err != nil {
-		return nil, err
-	}
-
-	if region, ok := d.GetOk("region"); ok && isValidRegionCode(region.(string)) {
-		os.Setenv("SCP_REGION", region.(string))
-		providerConfig.RegionCode = region.(string)
-		if !providerConfig.SupportVPC {
-			providerConfig.RegionNo = *regionCacheByCode[region.(string)].RegionNo
-		}
-    // Region을 String이 아닌, no로 변경하여 사용하는 것으로 추정된다. SCP에서의 REGION이 단순 String인 경우, if 구문을 삭제하는 것도 고려하기
-	} else {
-		return nil, fmt.Errorf("no region data for region_code `%s`. please change region_code and try again", region)
-	}
-
 	return &providerConfig, nil
 }
 
@@ -115,9 +51,6 @@ func init() {
 	descriptions = map[string]string{
 		"access_key":  "Access key of scp",
 		"secret_key":  "Secret key of scp",
-		"region":      "Region of scp",
-		"site":        "Site of scp (dev / stg)",
-		"support_vpc": "Support VPC platform", // 해당 argument의 정체가 무엇인지 모른다. 
 	}
 }
 
